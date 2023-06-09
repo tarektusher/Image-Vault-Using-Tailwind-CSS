7ce9ad2fa7b1b82d321570deeb431f5239572fbb
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2023-06-10 02:07:33 +0600

    index and style file update now

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+       colors: {
+        "top-nav" :"#232228",
+        "sub-nav" :"#35353d",
+        "f-color":"#FFF",
+        "body-color":" #3c3c44",
+        "temp-color":"#3C3C43",
+        "new-color" : "#C5C5C5",
+        "sel-bl" : "#0095D9",
+    }
+    },
+   
+  },
+  plugins: [],
+}
+
